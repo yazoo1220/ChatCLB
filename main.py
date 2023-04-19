b49f229b9bc7e715871912b955cbf11cdfe60cf7
yazoo1220 <podman1220@hotmail.com>
2023-04-19 16:38:09 +0300

    disabled user_input refresh

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if ask_button:
         chat_history = []
         st.session_state.past.append(user_input)
         st.session_state.generated.append(qa({"question": user_input, "chat_history": chat_history})['answer'])
-        user_input.value = ""
+#         user_input.value = ""
 
 if st.session_state["generated"]:
 
