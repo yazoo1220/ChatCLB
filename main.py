d90e187aaa72a168fed4ec06918c7bce5f07aa37
yazoo1220 <podman1220@hotmail.com>
2023-04-19 16:35:11 +0300

    added checkbox for gpt4

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if "generated" not in st.session_state:
     
 st.caption("NB!:this is currently beta version and only knows about 'schedulers' part of Calabrio.")
 st.caption("https://help.calabrio.com/doc/Content/web/roles-scheduler.htm")
+is_gpt4 = st.checkbox('Enable GPT4',help="With this it might get slower")
 
 if "past" not in st.session_state:
     st.session_state["past"] = []
@@ -38,7 +39,11 @@ def get_chat_history(inputs) -> str:
     return "\n".join(res)
 @st.cache_resource
 def create_qa():
-    llm = ChatOpenAI(temperature=0.9, model_name='gpt-4', streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True)
+    if is_gpt4:
+        model = "gpt-4"
+    else:
+        model = "gpt-3.5-turbo"
+    llm = ChatOpenAI(temperature=0.9, model_name=model, streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True)
     embeddings = OpenAIEmbeddings()
 #     index_name = os.getenv("PINECONE_INDEX_NAME")
     db = Pinecone.from_existing_index(index_name='calabrio',embedding=embeddings)
